cc2ed2a588d5cf57a3c4056e0d8c403e3606115f
Laiba Tahir <182256201+laibatahir27@users.noreply.github.com>
2025-12-24 00:30:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-require('dotenv').config();
+//require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -90,3 +90,4 @@ app.post('/contacts', async (req, res) => {
 app.listen(PORT, () => {
   console.log(` Server running on http://localhost:${PORT}`);
 });
+
